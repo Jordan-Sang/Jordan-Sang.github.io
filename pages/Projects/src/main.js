888b1b99e64822f5c91bdb726a0c2e7be0261bb3
Jordan Sang <40538794+Jordan8888@users.noreply.github.com>
2020-07-20 14:02:43 -0400

    Change project descriptions

--- pages/Projects/src/main.js
+++ pages/Projects/src/main.js
@@ -8,12 +8,12 @@ var context = {
     projects: [{
         title: 'General Knowledge Quiz',
         date: 'July 5, 2020',
-        description: 'A general knowledge quiz powered by vanilla JavaScript and the Handlebars.js library',
+        description: 'A trivia quiz powered by the Handlebars.js library and vanilla JavaScript',
         link: 'project-folders/01 - General Knowledge Quiz/index.html'
     }, {
         title: 'Calculator',
         date: 'July 11, 2020',
-        description: 'A simple JavaScript calculator',
+        description: 'A simple calculator that operates with vanilla JavaScript',
         link: 'project-folders/02 - JavaScript Calculator/index.html'
     }]
 };
