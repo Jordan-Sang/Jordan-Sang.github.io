8c89f6ac36f19712f4874f8ce029a6fe692914de
Jordan Sang <40538794+Jordan8888@users.noreply.github.com>
2020-07-21 08:03:34 -0400

    Reorganize files

--- resources/Projects/main.js
+++ resources/Projects/main.js
@@ -9,12 +9,12 @@ var context = {
         title: 'General Knowledge Quiz',
         date: 'July 5, 2020',
         description: 'A trivia quiz powered by the Handlebars.js library and vanilla JavaScript',
-        link: 'project-folders/01 - General Knowledge Quiz/index.html'
+        link: './resources/Projects/01 - General Knowledge Quiz/index.html'
     }, {
         title: 'Calculator',
         date: 'July 11, 2020',
         description: 'A simple calculator that operates with vanilla JavaScript',
-        link: 'project-folders/02 - JavaScript Calculator/index.html'
+        link: './resources/Projects/02 - JavaScript Calculator/index.html'
     }]
 };
 
