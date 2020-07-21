35f3f4821a77d84e4f3842ac498c45c841bd76aa
Jordan Sang <40538794+Jordan8888@users.noreply.github.com>
2020-07-21 08:21:31 -0400

    Move projects

--- scripts/projects.js
+++ scripts/projects.js
@@ -9,12 +9,12 @@ var context = {
         title: 'Calculator',
         date: 'July 11, 2020',
         description: 'A simple calculator that operates with vanilla JavaScript',
-        link: './resources/Projects/02 - JavaScript Calculator/index.html'
+        link: './projects/02-JavaScript-Calculator/index.html'
     }, {
         title: 'General Knowledge Quiz',
         date: 'July 5, 2020',
         description: 'A trivia quiz powered by the Handlebars.js library and vanilla JavaScript',
-        link: './resources/Projects/01 - General Knowledge Quiz/index.html'
+        link: './projects/01-General-Knowledge-Quiz/index.html'
     }]
 };
 
