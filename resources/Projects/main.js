4e592ad2c5c65c13c6e475732f69777d5ef50c58
Jordan Sang <40538794+Jordan8888@users.noreply.github.com>
2020-07-21 08:08:37 -0400

    Change project order

--- resources/Projects/main.js
+++ resources/Projects/main.js
@@ -6,15 +6,15 @@ var template = Handlebars.compile(sourceContent);
 
 var context = {
     projects: [{
-        title: 'General Knowledge Quiz',
-        date: 'July 5, 2020',
-        description: 'A trivia quiz powered by the Handlebars.js library and vanilla JavaScript',
-        link: './resources/Projects/01 - General Knowledge Quiz/index.html'
-    }, {
         title: 'Calculator',
         date: 'July 11, 2020',
         description: 'A simple calculator that operates with vanilla JavaScript',
         link: './resources/Projects/02 - JavaScript Calculator/index.html'
+    }, {
+        title: 'General Knowledge Quiz',
+        date: 'July 5, 2020',
+        description: 'A trivia quiz powered by the Handlebars.js library and vanilla JavaScript',
+        link: './resources/Projects/01 - General Knowledge Quiz/index.html'
     }]
 };
 
